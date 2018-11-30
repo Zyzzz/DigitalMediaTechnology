dde53cebe16143767c4d3cb90c9cd76de9ec6f1e
“antonialq” <1649794366@qq.com>
2018-11-30 11:21:50 +0800

    完善第三个页面

--- Ui/Libraries/Widgets/ContentWidget.py
+++ Ui/Libraries/Widgets/ContentWidget.py
@@ -13,7 +13,7 @@ from random import randint
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget, QProgressBar, QVBoxLayout,QLabel,QStackedWidget,QMainWindow
 from Ui.Libraries.Widgets.SubWindows import SubWindows
-from Ui.Libraries.Widgets.SubWindows import charts
+from Ui.Libraries.Widgets import charts
 from Ui.Libraries.Widgets.MainQWindows import MainWindows
 # from Faster_RCNN import test
 __Author__ = "By: Irony.\"[讽刺]\nQQ: 892768447\nEmail: 892768447@qq.com"
@@ -49,7 +49,7 @@ class ContentWidget(QWidget):
         chartWindow = QMainWindow()
         ui = charts.Ui_MainWindow()
         ui.setupUi(chartWindow)
-        chartWindow.setStyleSheet(open("themes/default/style.qss",
+        chartWindow.setStyleSheet(open("Ui/themes/default/style.qss",
                                 "rb").read().decode("utf-8"))
         self.stackedWidget.addWidget(chartWindow)
         # for i in range(3):
